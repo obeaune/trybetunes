d1f520c04e931aaa8acb1d2e5ab8b4d31cb67765
obeaune <linecora86@gmail.com>
2022-02-14 12:11:50 -0300

    Requisito 8

--- src/components/MusicCard.js
+++ src/components/MusicCard.js
@@ -1,15 +1,38 @@
 import React, { Component } from 'react';
-import PropTypes, { string } from 'prop-types';
+import PropTypes, { string, number } from 'prop-types';
+import { addSong } from '../services/favoriteSongsAPI';
+import Loading from './Loading';
 
 class MusicCard extends Component {
+  state = {
+    loading: false,
+  }
+
+  handleCheckboxChange = async (track) => {
+    this.setState({ loading: true });
+    await addSong(track);
+    this.setState({ loading: false });
+  }
+
   render() {
-    const { track: { trackName, previewUrl } } = this.props;
+    const { loading } = this.state;
+    const { track } = this.props;
+    const { trackName, previewUrl, trackId } = track;
 
     return (
       <div>
 
         <div>
           <h4>{ trackName }</h4>
+          <label htmlFor={ trackId }>
+            Favorita:
+            <input
+              onChange={ () => this.handleCheckboxChange(track) }
+              type="checkbox"
+              id={ trackId }
+              data-testid={ `checkbox-music-${trackId}` }
+            />
+          </label>
         </div>
 
         <audio src={ previewUrl } data-testid="audio-component" controls>
@@ -17,6 +40,7 @@ class MusicCard extends Component {
           O seu navegador não suporta o elemento.
           <code>audio</code>
         </audio>
+        {loading && <Loading />}
 
       </div>
     );
@@ -27,6 +51,7 @@ MusicCard.propTypes = {
   track: PropTypes.shape({
     trackName: string,
     previewUrl: string,
+    trackId: number,
   }).isRequired,
 };
 
--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
+import Loading from '../components/Loading';
 
 class Favorites extends Component {
+  state = {
+    loading: false,
+  }
+
   render() {
+    const { loading } = this.state;
+
     return (
       <div data-testid="page-favorites">
         <Header />
+        {loading && <Loading />}
       </div>
     );
   }
