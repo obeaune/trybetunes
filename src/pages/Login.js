6f6d03028febc77c776b019238bb169a61f7ed13
obeaune <linecora86@gmail.com>
2022-02-13 22:29:33 -0300

    Requisito 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,61 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import { createUser } from '../services/userAPI';
+import Loading from '../Loading';
 
 class Login extends Component {
+  state = {
+    inputUser: '',
+    loading: false,
+    isUserValid: false,
+  };
+
+  loginButtonClick = async (event) => {
+    event.preventDefault();
+    const { inputUser } = this.state;
+    this.setState({
+      loading: true,
+    });
+    await createUser({ name: inputUser });
+    this.setState({
+      isUserValid: true,
+    });
+  };
+
+  verifyUserLength = (text) => {
+    if (text.length <= 2) return true;
+  }
+
   render() {
+    const { inputUser, loading, isUserValid } = this.state;
+
     return (
-      <div data-testid="page-login" />
+      <form data-testid="page-login">
+        <label htmlFor="inputUser">
+          Nome:
+          <input
+            data-testid="login-name-input"
+            id="inputUser"
+            type="text"
+            value={ inputUser }
+            onChange={ ({ target }) => this.setState({ inputUser: target.value }) }
+          />
+        </label>
+
+        <button
+          data-testid="login-submit-button"
+          id="isLoginButtonDisabled"
+          type="submit"
+          onClick={ this.loginButtonClick }
+          disabled={ this.verifyUserLength(inputUser) }
+        >
+          Entrar
+        </button>
+
+        {loading && <Loading />}
+        {isUserValid && <Redirect to="/search" />}
+
+      </form>
     );
   }
 }
